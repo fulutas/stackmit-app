29658e071cd9207f9c5be7bff0e8b4df8832b8cb
Furkan Ulutaş <ulutasfurkan@gmail.com>
2025-05-30 18:15:40 +0300

    Enhance DirectoryList and ProjectVersionChangesModal with new features and improved error handling; add refresh and update functionality in IPC events.

--- app/pages/project-git-control/DirectoryList.tsx
+++ app/pages/project-git-control/DirectoryList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { FaGitAlt, FaFolder, FaCodeBranch } from "react-icons/fa";
 import { BiLogoVisualStudio } from "react-icons/bi";
 import { IoLink } from "react-icons/io5";
@@ -8,8 +8,10 @@ import ProjectVersionChangesModal from "./ProjectVersionChangesModal";
 import { FiSend } from "react-icons/fi";
 import SendBatchCommitModal from "./SendBatchCommitModal";
 import { toast } from "sonner";
-import { ipcRenderer } from "electron";
-
+import classNames from "classnames";
+import { IoRefresh } from "react-icons/io5";
+import { FaCodePullRequest, FaCheck } from "react-icons/fa6";
+import { FaGit } from "react-icons/fa";
 export interface DirectoryInfo {
   path: string;
   name: string;
@@ -25,23 +27,23 @@ export interface DirectoryInfo {
     error?: string;
   }>;
 }
-
 interface Props {
   directories: DirectoryInfo[];
   setDirectories: React.Dispatch<React.SetStateAction<DirectoryInfo[]>>;
 }
-
 const DirectoryList: React.FC<Props> = ({ directories, setDirectories }) => {
+  const repoRefreshButtonRef = useRef<HTMLButtonElement>(null);
+
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const [repoRefreshStatus, setRepoRefreshStatus] = useState<Record<string, string>>({});
 
   const [filterPendingChanges, setFilterPendingChanges] = useState<boolean>(false);
   const [filterGitRepo, setFilterGitRepo] = useState<boolean>(false);
   const [filterSelectAllChanged, setFilterSelectAllChanged] = useState<boolean>(false);
 
   const [changeModalOpen, setChangeModalOpen] = useState<boolean>(false);
-  const [changeModalDetail, setChangeModalDetail] = useState<DirectoryInfo | Object>({});
+  const [changeModalDetail, setChangeModalDetail] = useState<DirectoryInfo | Object | null>(null);
   const [batchCommitModalOpen, setBatchCommitModalOpen] = useState<boolean>(false);
-
   const [selectedDirectories, setSelectedDirectories] = useState<string[]>([]);
 
   const filteredDirectories = directories.filter((dir) => {
@@ -60,7 +62,7 @@ const DirectoryList: React.FC<Props> = ({ directories, setDirectories }) => {
       const changedPaths = filteredDirectories
         .filter((dir) => dir.pendingChanges)
         .map((dir) => dir.path);
-
+      console.log(changedPaths)
       setSelectedDirectories((prev) => {
         const merged = [...new Set([...prev, ...changedPaths])];
         return merged;
@@ -77,7 +79,6 @@ const DirectoryList: React.FC<Props> = ({ directories, setDirectories }) => {
         : [...prev, path] // ekle
     );
   };
-
   const openBatchCommit = () => {
     if (selectedDirectories.length === 0) {
       toast.error('Please select at least one directory.');
@@ -87,52 +88,110 @@ const DirectoryList: React.FC<Props> = ({ directories, setDirectories }) => {
     setBatchCommitModalOpen(true);
 
   }
-
   const sendBatchCommit = async (commitMessage: string) => {
-    // ipcRenderer.send('send-commit', { directories: selectedDirectories, commitMessage });
-    const directoryList = await window.gitLib.sendCommit({ directories: selectedDirectories, commitMessage });
+    if (!commitMessage || commitMessage.trim() === '') {
+      toast.error('Commit message cannot be empty.');
+      return;
+    }
 
-    console.log(directoryList)
-    // try {
-    //   const results = await ipcRenderer.invoke('send-commit', {
-    //     directories: selectedDirectories,
-    //     commitMessage
-    //   });
+    try {
+      const response = await window.gitLib.sendCommit({ directories: selectedDirectories, commitMessage });
+      console.log("sendBatchCommit Response -->", response);
 
-    //   console.log(results)
-    //   // // Dizinleri yenile
-    //   // directories = await ipcRenderer.invoke('select-directories');
-    //   // renderDirectories();
+      const successCount = response.filter(item => item.success).length;
+      const errorCount = response.length - successCount;
 
-    // } catch (error) {
-    //   console.error('Commit Error:', error);
-    //   alert('An error occurred during commit.');
-    // }
+      if (successCount > 0) {
+        toast.success(`${successCount} project${successCount > 1 ? 's' : ''} were committed successfully.`);
+      }
+
+      if (errorCount > 0) {
+        toast.error(`${errorCount} project${errorCount > 1 ? 's' : ''} failed to commit due to an error.`);
+      }
+    } catch (error) {
+      console.error('Commit Error:', error);
+      toast.error('An unexpected error occurred. Commit process failed.');
+    }
   }
+  const refreshDirectories = async () => {
+    const allDirectories = directories.map(dir => dir.path);
+
+    try {
+      const directoryList = await window.gitLib.refreshDirectories(allDirectories)
+      setDirectories(directoryList);
+
+      if (directoryList.length > 0) {
+        toast.success('Folders successfully refreshed.');
+      } else {
+        toast.info('No directories found.');
+      }
+    } catch (error) {
+      console.error('Error refreshing directories:', error);
+      toast.error('An error occurred while refreshing directories.');
+    }
+  };
+  const repoCheckUpdates = async (dirPath: string) => {
+    try {
+      const updatedDir = await window.gitLib.repoCheckUpdates(dirPath);
+      console.log(updatedDir)
+
+      // Eğer güncel ve değişiklik yoksa buton yazısını değiştir
+      if (updatedDir.success && updatedDir.count === 0) {
+        setRepoRefreshStatus((prev) => ({ ...prev, [dirPath]: 'Up to date' }));
+
+        // 2 saniye sonra tekrar 'Refresh' olarak değiştir
+        setTimeout(() => {
+          setRepoRefreshStatus((prev) => {
+            const updated = { ...prev };
+            delete updated[dirPath];
+            return updated;
+          });
+        }, 2000);
+      }
+
+      setDirectories((prev) =>
+        prev.map((dir) => (dir.path === updatedDir.path ? updatedDir : dir))
+      );
+      toast.success('Repository updates checked successfully.');
+    } catch (error) {
+      console.error('Error checking repository updates:', error);
+      toast.error('An error occurred while checking repository updates.');
+    }
+  };
 
   return (
     <>
       <div className="p-6">
         <div className="flex justify-between items-baseline">
           <h1 className="w-full text-3xl text-white font-bold mb-6">Projects ({filteredDirectories.length})</h1>
+          {/* Directories Action Buttons */}
           {directories.length > 0 && (
-            <button
-              onClick={() => openBatchCommit()}
-              className="flex gap-2 min-w-[200px] cursor-pointer justify-center items-center mt-4 px-4 py-2 bg-gray-900 text-white text-sm rounded-lg hover:opacity-90 transition">
-              <FiSend size={16} />
-              Send Batch Commit
-            </button>
+            <div className="flex gap-3">
+              <button
+                onClick={() => refreshDirectories()}
+                className="flex gap-2  cursor-pointer justify-center items-center mt-4 px-4 py-2 bg-green-900 text-white text-sm rounded-lg hover:opacity-90 transition">
+                <IoRefresh size={16} />
+                Refresh
+              </button>
+              <button
+                onClick={() => openBatchCommit()}
+                className="flex gap-2 min-w-[200px] cursor-pointer justify-center items-center mt-4 px-4 py-2 bg-gray-900 text-white text-sm rounded-lg hover:opacity-90 transition">
+                <FiSend size={16} />
+                Send Batch Commit
+              </button>
+            </div>
           )}
+          {/* Directories Action Buttons */}
         </div>
         <input
           type="text"
-          placeholder="Search repositories..."
+          placeholder="Search..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           className="mb-6 w-full p-3 rounded-xl border-2 border-gray-700 focus:outline-none focus:ring-0 focus:ring-blue-400 dark:bg-gray-800 dark:text-white dark:border-gray-600"
         />
-
-        {filteredDirectories.length > 0 && (
+        {/* Filters */}
+        {directories.length > 0 && (
           <div id="filters" className="mb-6 flex gap-4">
             <h2 className="text-white font-medium">Filters:</h2>
             <div className="flex gap-5">
@@ -166,20 +225,24 @@ const DirectoryList: React.FC<Props> = ({ directories, setDirectories }) => {
             </div>
           </div>
         )}
+        {/* Filters */}
 
-        <div className="space-y-4">
+        {/* DirectoriesList */}
+        <div className="directories-list space-y-4">
           {filteredDirectories.length > 0 && filteredDirectories.map((dir, index) => (
             <div
               key={dir.path}
               data-index={index}
-              className="rounded-2xl border border-gray-200 dark:border-gray-700 p-5 shadow-md hover:shadow-lg transition-shadow bg-white dark:bg-gray-900"
+              className="directory-item rounded-2xl border border-gray-200 dark:border-gray-700 p-5 shadow-md hover:shadow-lg transition-shadow bg-white dark:bg-gray-900"
             >
               <div className="flex items-start justify-between">
                 <div className="flex gap-4">
                   <input
                     type="checkbox"
-                    className="mt-1 cursor-pointer accent-blue-600 w-5 h-5"
+                    className={classNames('mt-1 cursor-pointer accent-blue-600 w-5 h-5', { 'opacity-40 !cursor-not-allowed': !dir.isGitRepo || !dir.fileDiffs.length })}
                     checked={selectedDirectories.includes(dir.path)}
+                    title={!dir.isGitRepo || !dir.fileDiffs.length ? 'No git repository or no file diffs available' : 'Select this directory'}
+                    disabled={!dir.isGitRepo || !dir.fileDiffs.length}
                     onChange={() => directoriesCheckboxChange(dir.path)}
                   />
                   <div className="flex flex-col">
@@ -191,9 +254,12 @@ const DirectoryList: React.FC<Props> = ({ directories, setDirectories }) => {
                       className="flex gap-2 items-center text-sm text-gray-400 hover:underline cursor-pointer">
                       <FaFolder /> {dir.path}
                     </p>
-
                     {dir.isGitRepo && (
                       <div className="mt-5 space-y-1">
+                        <div className="flex gap-1 text-sm">
+                          <span className="font-medium flex gap-1 items-center"> <FaGit /> Remote Status: </span>
+                          <label className={classNames('text-sm font-medium', { 'text-green-500': dir.isGitRepo, 'text-red-500': !dir.isGitRepo })}>{dir.isGitRepo ? "Git Connected" : "No Git Connection"}</label>
+                        </div>
                         <div className="flex gap-1 text-sm">
                           <span className="font-medium flex gap-1 items-center"> <FaCodeBranch /> Current Branch: </span>
                           {dir.currentBranch}
@@ -224,19 +290,9 @@ const DirectoryList: React.FC<Props> = ({ directories, setDirectories }) => {
                   </div>
                 </div>
                 <div className="flex flex-col items-start">
-                  {dir.isGitRepo ? (
-                    <button className="flex gap-2 items-center mt-4 px-4 py-2 w-full bg-green-600 text-white text-sm rounded-lg hover:opacity-90 transition">
-                      <FaGitAlt size={20} />
-                      Git Connected
-                    </button>
-                  ) : (
-                    <button className="flex gap-2  items-centermt-4 px-4 py-2 w-full bg-red-500 text-white text-sm rounded-lg hover:opacity-90 transition">
-                      No Git Connection
-                    </button>
-                  )}
                   <button
                     onClick={() => window.api.openInVSCode(dir.path)}
-                    className="flex gap-2 items-center mt-4 px-4 py-2 w-full cursor-pointer bg-blue-600 text-white text-sm rounded-lg hover:bg-blue-700 transition"
+                    className="flex gap-2 items-center px-4 py-2 w-full cursor-pointer bg-blue-500 text-white text-sm rounded-lg hover:bg-blue-700 transition"
                   >
                     <BiLogoVisualStudio />
                     Open VSCode
@@ -255,13 +311,48 @@ const DirectoryList: React.FC<Props> = ({ directories, setDirectories }) => {
                   )}
                 </div>
               </div>
+              <hr className="mt-5 mb-5 h-0.5 border-t-0 bg-neutral-100 dark:bg-white/10" />
+              {/* Git Actions */}
+              <div className="git-actions flex flex-col mt-4">
+                <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200 mb-2">Git Actions</h3>
+                <div className="flex flex-row gap-2">
+                  <button
+                    onClick={() => repoCheckUpdates(dir.path)}
+                    className={classNames(
+                      "flex gap-2 cursor-pointer justify-center items-center px-4 py-2 text-white text-sm rounded-lg transition",
+                      repoRefreshStatus[dir.path] === 'Up to date' ? "bg-green-600 hover:bg-green-700" : "bg-gray-700 hover:opacity-90"
+                    )}
+                  >
+                    {repoRefreshStatus[dir.path] === 'Up to date' ? (
+                      <FaCheck size={16} />
+                    ) : (
+                      <IoRefresh size={16} />
+                    )}
+                    {repoRefreshStatus[dir.path] || 'Check Updates'}
+                  </button>
+                  <button className="flex gap-2 cursor-pointer justify-center items-center px-4 py-2 bg-gray-700 text-white text-sm rounded-lg hover:opacity-90 transition">
+                    <FaCodePullRequest size={16} />
+                    Pull
+                  </button>
+                </div>
+              </div>
+              {/* Git Actions */}
             </div>
           ))}
-          {filteredDirectories.length === 0 && <p className="text-center font-medium text-[15px] text-gray-200">Kayıt bulunamadı.</p>}
+
+          {filteredDirectories.length === 0 && <p className="py-3 bg-gray-700 rounded-md text-center font-medium text-[15px] text-gray-200">Record not found.</p>}
         </div>
+        {/* DirectoriesList */}
       </div>
 
-      {changeModalOpen && <ProjectVersionChangesModal changeModalDetail={changeModalDetail} setChangeModalDetail={setChangeModalDetail} changeModalOpen={changeModalOpen} setChangeModalOpen={setChangeModalOpen} />}
+      {changeModalOpen && (
+        <ProjectVersionChangesModal
+          changeModalDetail={changeModalDetail}
+          setChangeModalDetail={setChangeModalDetail}
+          changeModalOpen={changeModalOpen}
+          setChangeModalOpen={setChangeModalOpen}
+        />
+      )}
 
       {batchCommitModalOpen && (
         <SendBatchCommitModal
