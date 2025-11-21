faa231648b2ddeefbf6d7377b4ec04a51406fdb9
furkan.ulutas <furkan.ulutas@next4biz.com>
2025-11-21 12:04:17 +0300

    feat: update footer to include GitHub link and current year copyright

--- app/components/page-shared/Footer.tsx
+++ app/components/page-shared/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaGithub } from 'react-icons/fa'
 
 type Props = {}
 
@@ -10,40 +11,19 @@ const Footer = (props: Props) => {
     >
       <div className="container mx-auto flex flex-col px-4 text-center text-sm md:flex-row md:justify-between md:text-left lg:px-8 xl:max-w-7xl">
         <div className="pt-4 pb-1 md:pb-4">
-          <a
-            href="https://tailkit.com"
-            className="font-medium text-blue-600 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300"
-            target="_blank"
-          >
+          <span className="font-medium text-blue-600 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300">
             Stackmit.
-          </a>{" "}
-          ©
+          </span>{" "}
+          © {new Date().getFullYear()}
         </div>
         <div className="inline-flex items-center justify-center pt-1 pb-4 md:pt-4">
-          <span>Crafted with</span>
-          <svg
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            className="hi-solid hi-heart mx-1 inline-block size-4 text-red-600"
+          <a
+            href="https://github.com/fulutas/stackmit-app"
+            className="font-medium "
+            target="_blank"
           >
-            <path
-              fillRule="evenodd"
-              d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <span>
-            {" "}
-            by{" "}
-            <a
-              href="https://pixelcave.com"
-              className="font-medium text-blue-600 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300"
-              target="_blank"
-            >
-              FurkanUlutas.
-            </a>
-          </span>
+            <FaGithub size={20} />
+          </a>
         </div>
       </div>
     </footer>
